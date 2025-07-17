8d0d4ef5cf92a7dc0d314572a7e82f8d14862c2a
Matsvei <matvei200310@mail.ru>
2025-07-17 12:25:16 +0300

    edit: main script class edit - added app creation QtApplication; - added creation instances of a Model and View; - removed unused code;

--- app/src/main.py
+++ app/src/main.py
@@ -1,20 +1,17 @@
+from PyQt5.QtWidgets import QApplication as AppQT
 from controller import Controller
+from view import View
+from model import Model
+import sys
 
-
-# def setup_ui_events(window):
-#     window.Playlist1_Icon.mousePressEvent = lambda event: hide(window.VTV_Video2_Container)
-#     window.Playlist1_Icon.enterEvent = lambda event: led(window.Playlist1_Icon)
-#
-# def hide(group_box):
-#     group_box.setVisible(False)
-#
-# def led(group_box):
-#     group_box.setStyleSheet("border: 2px solid white;")
 
 
 if __name__ == "__main__":
-    controller = Controller()
+    app = AppQT([])
+    model = Model()
+    view = View()
+    controller = Controller(model, view)
     controller.initUi()
     controller.showUI()
-    controller.startApp()
+    sys.exit(app.exec_())
 
