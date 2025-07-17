bfc9bec6a5add5eb5ca1fc7712fcdf5763c2f68b
Matsvei <matvei200310@mail.ru>
2025-07-17 16:40:57 +0300

    edit: Controller class editing - __init__(self, model, view) edited parameters; - setupUISignals(self) added for handle user clicks on view front; - switchMainMenuTab(self, tab) added as handler of clicks on control buttons, which switch menu tabs;

--- app/src/controller.py
+++ app/src/controller.py
@@ -1,18 +1,30 @@
 from PyQt5 import QtWidgets
-
+from PyQt5.QtWidgets import QApplication as AppQT
 import sys
 
 class Controller:
     def __init__(self, model, view):
         self.view = view
         self.model = model
+        self.setupUI()
+        self.startUI()
 
-    def initUi(self):
-        self.view.setupMainWindow()
+    def setupUI(self):
         self.view.setupGraphicalEvents()
+        self.setupUISignals()
 
-    def showUI(self):
+    def startUI(self):
         self.view.showMainWindow(True)
+        sys.exit(self.view.app.exec_())
+
+    def setupUISignals(self):
+        self.view.SettingsTabButton.clicked.connect(lambda: self.switchMainMenuTab(self.view.SettingsTab))
+        self.view.FindTabButton.clicked.connect(lambda: self.switchMainMenuTab(self.view.FindTab))
+        self.view.PlaylistsTabButton.clicked.connect(lambda: self.switchMainMenuTab(self.view.ViewingTab))
+
+    def switchMainMenuTab(self, tab):
+        self.view.MainMenu.setCurrentWidget(tab)
+
 
 
 
