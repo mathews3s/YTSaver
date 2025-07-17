a46faedc691b90999e1149b345fc112a00eafc04
Matsvei <matvei200310@mail.ru>
2025-07-17 12:22:39 +0300

    edit: controller class edit - self.app parameter removed, app creation moved to main.py; - initUi(self) edit, added functional for setup events of view; - removed unused functions;

--- app/src/controller.py
+++ app/src/controller.py
@@ -1,20 +1,23 @@
-from view import View, QtWidgets
-from model import Model
+from PyQt5 import QtWidgets
+
 import sys
 
 class Controller:
-    def __init__(self):
-        self.app = QtWidgets.QApplication([])
-        self.view = View()
-        self.model = Model()
-
-    def startApp(self):
-        sys.exit(self.app.exec_())
+    def __init__(self, model, view):
+        self.view = view
+        self.model = model
 
     def initUi(self):
         self.view.setupMainWindow()
+        self.view.setupGraphicalEvents()
 
     def showUI(self):
         self.view.showMainWindow(True)
+
+
+
+    # def hide(self):
+    #     self.view.VTV_Video2_Container.setVisible(False)
+
 
 
