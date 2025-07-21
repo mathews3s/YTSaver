78024b77afb1bf93bcdd3b15d4b6c15066066fc8
Matsvei Uharenka <matvei200310@mail.ru>
2025-07-21 23:07:58 +0300

    feature: implementation of scrolling playlists on page - model.py: add methods for changing current playlists 1 and 2, calculating offset both off their in db, getting list of them from db; - controller.py: add methods for interaction model and view changes in context of playlists listing;

--- app/src/controller.py
+++ app/src/controller.py
@@ -1,40 +1,88 @@
-from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QApplication as AppQT
 import sys
 
+
 class Controller:
+
     def __init__(self, model, view):
         self.view = view
         self.model = model
-        self.id_of_first
-        self.model.get_firsts_playlist()
-        self.setupUI()
-        self.startUI()
+        self.setup_ui()
+        self.start_ui()
 
-    def setupUI(self):
+    def setup_ui(self):
         self.view.setupGraphicalEvents()
-        self.setupUISignals()
-
-    def startUI(self):
+        self.setup_ui_signals()
         self.view.showMainWindow(True)
+        self.update_view_tab()
+        self.switch_to_view_tab()
+
+    def start_ui(self):
         sys.exit(self.view.app.exec_())
 
-    def setupUISignals(self):
-        self.view.SettingsTabButton.clicked.connect(lambda: self.switchMainMenuTab(self.view.SettingsTab))
-        self.view.FindTabButton.clicked.connect(lambda: self.switchMainMenuTab(self.view.FindTab))
-        self.view.PlaylistsTabButton.clicked.connect(lambda: self.switchMainMenuTab(self.view.ViewingTab))
+    def setup_ui_signals(self):
+        # self.view.SettingsTabButton.clicked.connect(lambda: self.switchToSettingsTab())
+        self.view.FindTabButton.clicked.connect(lambda: self.switch_to_find_tab())
+        self.view.PlaylistsTabButton.clicked.connect(lambda: self.switch_to_view_tab())
 
-    def switchMainMenuTab(self, tab):
-        self.view.MainMenu.setCurrentWidget(tab)
+        self.view.VTP_UpButton.clicked.connect(lambda: self.playlists_scroll_up())
+        self.view.VTP_DownButton.clicked.connect(lambda: self.playlists_scroll_down())
+        self.view.VTV_UpButton.clicked.connect(lambda: self.videos_scroll_up())
+        self.view.VTV_DownButton.clicked.connect(lambda: self.videos_scroll_down())
 
-    def showPlaylists(self):
-        self.model.get_firsts_playlist()
+        self.view.Playlist1_Icon.mousePressEvent = lambda event: self.playlist1_icon_clicked()
+        self.view.Playlist2_Icon.mousePressEvent = lambda event: self.playlist2_icon_clicked()
 
+    def switch_to_view_tab(self):
+        self.view.MainMenu.setCurrentWidget(self.view.ViewingTab)
 
+    def update_view_tab(self):
+        count = self.model.get_playlists_count()
+        self.model.setup_current_playlists()
 
+        if count == 0:
+            self.view.VTP_NothingLabel.setVisible(True)
+        else:
+            self.view.VTP_NothingLabel.setVisible(False)
 
-    # def hide(self):
-    #     self.view.VTV_Video2_Container.setVisible(False)
+            playlists_info = self.model.get_current_playlists_info()
 
+            if not playlists_info[0] is None:
+                self.view.set_playlist1_info(playlists_info[0])
+                if playlists_info[2] == playlists_info[0]:
+                    self.view.highlightItem(self.view.Playlist1_Icon, self.view.px_medium)
+                else:
+                    self.view.unhighlightItem(self.view.Playlist1_Icon, self.view.px_medium)
+                self.view.VTP_Playlist1_Container.setVisible(True)
 
+            if not playlists_info[1] is None:
+                self.view.set_playlist2_info(playlists_info[1])
+                if playlists_info[2] == playlists_info[1]:
+                    self.view.highlightItem(self.view.Playlist2_Icon, self.view.px_medium)
+                else:
+                    self.view.unhighlightItem(self.view.Playlist2_Icon, self.view.px_medium)
+                self.view.VTP_Playlist2_Container.setVisible(True)
+            else:
+                self.view.VTP_Playlist2_Container.setVisible(False)
 
+        self.view.VT_PlaylistsCount.setText(str(count))
+
+    def switch_to_find_tab(self):
+        self.view.MainMenu.setCurrentWidget(self.view.FindTab)
+
+    def playlists_scroll_down(self):
+        self.model.playlists_offset_down()
+        self.update_view_tab()
+
+    def playlists_scroll_up(self):
+        self.model.playlists_offset_up()
+        self.update_view_tab()
+
+    def playlist1_icon_clicked(self):
+        self.model.set_playlist1_as_current()
+        self.view.highlightItem(self.view.Playlist1_Icon, self.view.px_medium)
+        self.view.unhighlightItem(self.view.Playlist2_Icon, self.view.px_medium)
+
+    def playlist2_icon_clicked(self):
+        self.model.set_playlist2_as_current()
+        self.view.highlightItem(self.view.Playlist2_Icon, self.view.px_medium)
+        self.view.unhighlightItem(self.view.Playlist1_Icon, self.view.px_medium)
--- app/src/model.py
+++ app/src/model.py
@@ -2,29 +2,117 @@ import sqlite3 as db
 import json
 
 
-
-
 class Model:
     def __init__(self):
         self.db = "../appData/database.db"
+
+        self.playlists = self.get_playlists_db()
+        self.playlists_count = self.get_playlists_count_db()
+        self.playlist_1 = None
+        self.playlist_2 = None
+        self.selected_playlist = None
+        self.offset_playlists = 0
+        self.setup_current_playlists()
+
+        self.videos = None
+        self.videos_count = None
+        self.current_video = None
+        self.video_1 = None
+        self.video_2 = None
+        self.offset_videos = 0
+
+    def get_playlists_count(self):
+        return self.playlists_count
+
+    def get_current_playlists_info(self):
+        return self.playlist_1, self.playlist_2, self.selected_playlist
+
+    def set_playlist1_as_current(self):
+        self.selected_playlist = self.playlist_1
+
+    def set_playlist2_as_current(self):
+        self.selected_playlist = self.playlist_2
+
+    def get_playlists_db(self):
+        try:
+            with db.connect(self.db) as connection:
+                cursor = connection.cursor()
+                cursor.execute('''
+                    SELECT 
+                    id, playlist_name, playlist_count_videos, 
+                    playlist_path, playlist_icon, playlist_date
+                    FROM 
+                    playlist
+                    ORDER BY playlist_name''')
+                playlists = cursor.fetchall()
+                print(playlists)
+                return playlists
+        except Exception as e:
+            print("Произошла ошибка во время выполнения операции вставки: %s", e)
+
+    def get_playlists_count_db(self):
+        try:
+            with db.connect(self.db) as connection:
+                cursor = connection.cursor()
+                cursor.execute('''
+                    SELECT COUNT(*)
+                    FROM 
+                    playlist''')
+                count = cursor.fetchone()[0]
+                print(count)
+                return count
+        except Exception as e:
+            print("Произошла ошибка во время выполнения операции вставки: %s", e)
+
+    def setup_current_playlists(self):
+        item1 = None
+        item2 = None
+        try:
+            item1 = self.playlists[0 + self.offset_playlists]
+            item2 = self.playlists[1 + self.offset_playlists]
+        except Exception as e:
+            print("Произошла ошибка во время выбора playlists", e)
+        if (item1 is None) and (not (item2 is None)):
+            self.playlist_1 = item2
+            self.playlist_2 = None
+        elif (item2 is None) and (not (item1 is None)):
+            self.playlist_1 = item1
+            self.playlist_2 = None
+        else:
+            self.playlist_1 = item1
+            self.playlist_2 = item2
+
+    def playlists_offset_up(self):
+        if self.offset_playlists > 0:
+            self.offset_playlists -= 2
+
+    def playlists_offset_down(self):
+        if self.offset_playlists < self.playlists_count - 1:
+            self.offset_playlists += 2
+            print(self.offset_playlists)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     def get_default_settings(self):
         with open('../appData/default-app-settings.json', 'r') as file:
             parsed = json.load(file)
         return parsed
-
-    def insert(self):
-         try:
-             with db.connect(self.db) as connection:
-                 cursor = connection.cursor()
-                 cursor.execute('''
-                     INSERT INTO SETTINGS_SET
-                     (SET_NAME, LANGUAGE, DASH_ALLOW, DEF_PATH)
-                     VALUES (?, ?, ?, ?)''',
-                 ("f1", "en", 1, "../../"))
-                 connection.commit()
-         except Exception as e:
-             print("Произошла ошибка во время выполнения операции вставки: %s", e)
 
     def insert_playlist_into_db(self, ):
         try:
@@ -42,34 +130,16 @@ class Model:
         except Exception as e:
             print("Произошла ошибка во время выполнения операции вставки: %s", e)
 
-    def get_icon_of_playlist(self, playlist):
-        pass
 
-    def get_two_playlist(self, id_first_pl):
-        try:
-            with db.connect(self.db) as connection:
-                cursor = connection.cursor()
-                cursor.execute('''
-                    SELECT 
-                    playlist_name, playlist_count_videos, 
-                    playlist_path, playlist_icon, playlist_date
-                    FROM playlist 
-                    WHERE id >= ? 
-                    LIMIT 2''', (id_first_pl,))
-                playlists = cursor.fetchall()
-                print(playlists)
-        except Exception as e:
-            print("Произошла ошибка во время выполнения операции вставки: %s", e)
-
-    def get_first_playlists_id(self):
-        try:
-            with db.connect(self.db) as connection:
-                cursor = connection.cursor()
-                cursor.execute('''
-                    SELECT id
-                    FROM playlist 
-                    LIMIT 2''', (id_first_pl,))
-                playlists = cursor.fetchall()
-                print(playlists)
-        except Exception as e:
-            print("Произошла ошибка во время выполнения операции вставки: %s", e)
+    def insert(self):
+         try:
+             with db.connect(self.db) as connection:
+                 cursor = connection.cursor()
+                 cursor.execute('''
+                     INSERT INTO SETTINGS_SET
+                     (SET_NAME, LANGUAGE, DASH_ALLOW, DEF_PATH)
+                     VALUES (?, ?, ?, ?)''',
+                 ("f1", "en", 1, "../../"))
+                 connection.commit()
+         except Exception as e:
+             print("Произошла ошибка во время выполнения операции вставки: %s", e)
